7dd10ef4cd3ba8b57e90ae76eb01327d27f94af2
Victor Efobi <victorefobi1@gmail.com>
2023-11-27 08:20:28 -0800

    Betty fixed

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - a function that checks if a list has a cycle
+ *
+ * @list: list to check
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	slow = fast = list;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
