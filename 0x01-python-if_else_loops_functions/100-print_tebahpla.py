d279e63208a6e2301b1e9e5fb751f4f5f6eb634b
Victor Efobi <victorefobi1@gmail.com>
2023-11-29 04:20:39 -0800

    Advanced Task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(26, 0, -1):
-    if i % 2 == 0:
-        print(chr(i + 96), end="")
-    else:
-        print(chr(i + 64), end="")
+        print(chr(i + 96) if i % 2 == 0 else chr(i + 64), end="")
