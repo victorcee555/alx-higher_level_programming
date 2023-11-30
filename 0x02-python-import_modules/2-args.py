d26687f80bc22f62e80332f40c4e19b1061ef5c3
Victor Efobi <victorefobi1@gmail.com>
2023-11-30 06:03:29 -0800

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@
 import sys
 
 if __name__ == "__main__":
-        arg_count = len(sys.argv)
-        if arg_count == 1:
-            print("0 arguments.")
-        elif arg_count == 2:
-            print("1 argument:")
-            print("1: {}".format(sys.argv[1]))
-        elif arg_count > 2:
-            print("{} arguments:".format(arg_count - 1))
-            for i in range(len(sys.argv) - 1):
-                print("{}: {}".format(i+1, sys.argv[i+1]))
+    arg_count = len(sys.argv)
+    if arg_count == 1:
+        print("0 arguments.")
+    elif arg_count == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    elif arg_count > 2:
+        print("{} arguments:".format(arg_count - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{}: {}".format(i+1, sys.argv[i+1]))
