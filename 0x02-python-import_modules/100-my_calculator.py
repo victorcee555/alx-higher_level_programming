0e1d53e50beaa8393ee22f820cebb545c4f4b27d
Victor Efobi <victorefobi1@gmail.com>
2023-11-30 09:21:49 -0800

    Advanced Task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def calc(a, op, b):
+    if op == '+':
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+
+op = ['+', '-', '*', '/']
+
+arg_count = len(sys.argv) - 1
+if arg_count != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[3])
+if sys.argv[2] in op:
+    calc(num1, sys.argv[2], num2)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
