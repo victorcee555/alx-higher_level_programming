dd10ba1f2d2329fd61e4cb7fea30c56e736caf68
Victor Efobi <victorefobi1@gmail.com>
2023-12-04 15:12:55 -0800

    Empty string check added

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if ((my_list) and (idx < 0 or idx > len(my_list) - 1)):
+    if not my_list or idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
         del my_list[idx]
