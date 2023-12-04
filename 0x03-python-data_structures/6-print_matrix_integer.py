48670bd06f9a1063caff41cce37831909f62fe1d
Victor Efobi <victorefobi1@gmail.com>
2023-12-04 13:40:06 -0800

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,9 +4,10 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0 or all(not row for row in matrix):
         print()
         return matrix
-
-    for i in range(3):
-        for j in range(3):
+    row = len(matrix)
+    column = len(matrix[0])
+    for i in range(row):
+        for j in range(column):
             if j == 2:
                 print("{:d}".format(matrix[i][j]), end="")
             else:
