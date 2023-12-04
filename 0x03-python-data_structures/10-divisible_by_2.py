7db57ae0e868f4fad58eb0ab7d1d9dc1699cacd7
Victor Efobi <victorefobi1@gmail.com>
2023-12-04 14:52:51 -0800

    Task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list = []):
+    if my_list:
+        new_list = []
+        i = 0
+        while i < len(my_list):
+            if my_list[i] % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+            i += 1
+        else:
+            return new_list
