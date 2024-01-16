b7a9127d5b154257deeeed97af521c8a2a130be0
Victor Efobi <victorefobi1@gmail.com>
2024-01-16 05:52:04 -0800

    Task 0 (Function that squares the value of all integers of a matrix)

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@
 def square_matrix_simple(matrix=[]):
     second_matrix = []
     for i in matrix:
-        second_matrix.append(list(map(lambda x:x**2, i)))
+        second_matrix.append(list(map(lambda x: x**2, i)))
 
     return second_matrix
