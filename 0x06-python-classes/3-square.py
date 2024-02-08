797584f972050048a8bbc9c859bc004540a2e44f
Victor <victorefobi1@gmail.com>
2024-02-08 03:51:23 -0800

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module demonstrates a class with
+private instance attribute: size."""
+
+
+class Square:
+    """This is a class 'Square' that defines
+    a square by:(based on 1-square.py)"""
+
+    def __init__(self, size=0):
+        """This is an instance with private
+        instace attribute: size"""
+
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """This is an instance that returns
+        the current square area"""
+
+        return self.__size ** 2
